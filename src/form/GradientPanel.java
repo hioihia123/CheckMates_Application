3ea5c4562c9bf38206d41d8fce5c19de4c9251bc
Nguyen Phung <phungdangnguyen890@gmail.com>
2025-03-21 15:17:32 -0500

    Fix UI of dashboard

---
+++ src/form/GradientPanel.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package form;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.RenderingHints;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.Dimension;
+
+
+import javax.swing.UIManager;
+import javax.swing.BorderFactory;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import org.json.JSONObject; // Make sure to include a JSON library
+/**
+ *
+ * @author nguyenp
+ */
+class GradientPanel extends JPanel {
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D) g;
+        int width = getWidth();
+        int height = getHeight();
+        Color color1 = new Color(255, 255, 255);
+        Color color2 = new Color(230, 230, 250);
+        g2d.setPaint(new GradientPaint(0, 0, color1, width, height, color2));
+        g2d.fillRect(0, 0, width, height);
+    }
+}
+
